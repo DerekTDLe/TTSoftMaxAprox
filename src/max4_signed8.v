9b0c2698fd30ab2c8bd3ae933d5d46db5d824129
Derek Le <72225336+DerekTDLe@users.noreply.github.com>
2026-03-04 02:46:44 +0000

    fixing bugs

--- src/max4_signed8.v
+++ src/max4_signed8.v
@@ -6,10 +6,16 @@
 `default_nettype none
 
 module max4_signed8( //find the maximum of 4 signed 8-bit numbers
-    input  wire [7:0] a_i, b_i, c_i, d_i,
+    input wire [7:0] a_i, b_i, c_i, d_i,
     output wire [7:0] max_val_o
 );
-    wire signed [7:0] max_ab = (a_i > b_i) ? a_i : b_i;
-    wire signed [7:0] max_cd = (c_i > d_i) ? c_i : d_i;
+    // Convert inputs to signed for comparison
+    wire signed [7:0] a_s = a_i;
+    wire signed [7:0] b_s = b_i;
+    wire signed [7:0] c_s = c_i;
+    wire signed [7:0] d_s = d_i;
+
+    wire signed [7:0] max_ab = (a_s > b_s) ? a_s : b_s;
+    wire signed [7:0] max_cd = (c_s > d_s) ? c_s : d_s;
     assign max_val_o = (max_ab > max_cd) ? max_ab : max_cd;
 endmodule
--- src/sum4_tree.v
+++ src/sum4_tree.v
@@ -9,7 +9,7 @@ module sum4_tree( //Combinational adder tree (registered).
     input  wire [7:0] a_i, b_i, c_i, d_i,
     output reg  [7:0] sum_o
 );
-    always @(*) begin
+    always @(*) begin // WIP
         sum_o = a_i + b_i + c_i + d_i;
     end
     
